1bcbeb587dcdfffc74b2311e5c0f15b68aca0e65
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2026-03-25 19:09:11 +0100

    nushell: new Home Manager module
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

---
+++ modules/home/nushell.nix
@@ -0,0 +1,23 @@
+{
+  configurations.home.modules.nushell = { pkgs, ... }: {
+    programs.nushell = {
+      enable = true;
+
+      extraConfig = ''
+        alias l = ${pkgs.eza} --icons --no-quotes --group-directories-first
+        alias ll = l --binary --long --header
+        alias la = ll --all
+        
+        ${pkgs.fastfetch}
+      '';
+
+      settings = {
+        env.config = {
+          show_banner = false;
+          use_kitty_protocol = true;
+          rm.always_trash = true;
+        };
+      };
+    };
+  };
+}
