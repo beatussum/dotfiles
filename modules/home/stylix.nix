92b99364f366dee59567a048bd4df7c129e76c66
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2026-03-27 16:28:56 +0100

    stylix: disable KDE and GNOME support
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -2,8 +2,14 @@
   flake.homeModules.stylix =
     { pkgs, ... }:
     {
-      gtk.gtk4.theme = null;
-      stylix.enable = true;
-      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      stylix = {
+        enable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+        targets = {
+          gnome.enable = false;
+          kde.enable = false;
+        };
+      };
     };
 }
